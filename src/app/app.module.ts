7acd5cb58d27540edb29f8bd81409780246d7121
cleverbek1991 <dilshodnur@hotmail.com>
2022-07-13 09:34:50 -0500

    5 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CustomInputComponent } from './custom-input/custom-input.component';
+import { CurrencyInputComponent } from './currency-input/currency-input.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, ReactiveFormsModule ],
-  declarations: [ AppComponent, CustomInputComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
+  declarations: [AppComponent, CustomInputComponent, CurrencyInputComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
